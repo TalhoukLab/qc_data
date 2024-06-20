6931d1c3381d28bb2ecde7c5c7bce1b397dab209
kashish1928 <kashishjoshipura@gmail.com>
2024-06-20 15:31:05 -0700

    Add function for config.R file (initial stage)

--- R/config.R
+++ R/config.R
@@ -1,12 +1,32 @@
-# values
-ALIVE = "A"
-DECEASED = "D"
-EMPTY = c("", " ", "  ", NULL)
+#' Configuration Function
+#'
+#' @param data_file the name (in quotes "") of the data frame undergoing QC
+#'
+#' @return List of paths and values
+#' @export
+#'
+#' @examples
+config <- function(data_file) {
+  # Values
+  ALIVE <- "A"
+  DECEASED <- "D"
+  EMPTY <- c("", " ", "  ", NULL)
 
-# path
-yml_path <- paste0(getwd(), "/yml/")
-data_path <- paste0(getwd(), "/data/")
-# data_file <- "q20-00031_disease_details.csv"
-data_file <- "Q2024-4250936_disease_details.csv"
-data <- paste0(data_path, "/", data_file)
-rule_path <- paste0(getwd(), "/rules/")
+  # Paths
+  yml_path <- file.path(getwd(), "yml")
+  data_path <- file.path(getwd(), "data")
+  data <- file.path(data_path, data_file)
+  rule_path <- file.path(getwd(), "rules")
+
+  # Return a list of paths and values
+  list(
+    ALIVE = ALIVE,
+    DECEASED = DECEASED,
+    EMPTY = EMPTY,
+    yml_path = yml_path,
+    data_path = data_path,
+    data_file = data_file,
+    data = data,
+    rule_path = rule_path
+  )
+}
